ec91cfefee95dd23bce76b78d6479e1b28d94e3b
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-12 19:39:32 +0200

    initialize project files

---
+++ 06.loops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Diagnostics.Tracing;
+using System.Threading;
+
+namespace _07.loops
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+          
+        }
+
+
+    }
+
+
+}
